a8a99f4a25a79043fd326d3d227e7f67336f5212
jessenia <jessenia.felix@gmail.com>
2021-11-27 12:34:16 -0500

    working with different functions - self destruct, receive, withdraw, fallback

--- FunctionsExample.sol
+++ FunctionsExample.sol
@@ -2,34 +2,44 @@ pragma solidity ^0.8.9;
 
 contract FunctionsExample {
 
-    mapping(address => uint) public banaceReceived;
+    mapping(address => uint) public balanceReceived;
 
-    address payable owner;
+    address public owner;
 
-    constructor() public {
+    constructor() public payable {
         owner = msg.sender;
     }
 
-    function destroySmartContract() public {
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+    
+    function convertWeiToEther(uint _amountInWei) public pure returns(uint) {
+        return _amountInWei / 1 ether;
+    }    
+
+    function destroySmartContract(address payable _to) public {
         require(msg.sender == owner, "You are not the owner");
-        selfdestruct(owner);
+        selfdestruct(_to);
     }
 
     function receiveMoney() public payable {
-        assert(balanceReceived[msg.sender] + msg.value >= balanceReceive[msg.sender]);
-        balanceReiceived[msg.sender] += msg.value;
+        assert(balanceReceived[msg.sender] + msg.value >= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += msg.value;
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
         require(_amount <= balanceReceived[msg.sender], "not enough funds.");
-        assert(balanceRecived[msg.sender] >= balanceReived[msg.sender] - _amount);
+        assert(balanceReceived[msg.sender] >= balanceReceived[msg.sender] - _amount);
         balanceReceived[msg.sender] -= _amount;
         _to.transfer(_amount);
     }
 
-    function () external payable {
+    fallback() external payable {
         receiveMoney();
     }
 
+    receive() external payable {
 
+    }
 }
