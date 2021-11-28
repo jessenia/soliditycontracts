70c46bc007b0ba269695b75b1c1fba13aff8f8e2
jessenia <jessenia.felix@gmail.com>
2021-11-28 16:15:24 -0500

    using libraries

---
+++ LibrariesExample.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.9;
+
+import "./SafeMath.sol";
+
+// THIS IMPORT STATEMENT IS NOT WORKING SO I CREATED A LOCAL COPY
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+contract LibrariesExample {
+
+    using SafeMath for uint;
+
+    mapping(address => uint) public tokenBalance;
+
+    constructor() public {
+        tokenBalance[msg.sender] = 1;
+    }
+
+    function sendToken(address _to, uint _amount) public returns (bool) {
+        tokenBalance[msg.sender] = tokenBalance[msg.sender].sub(_amount);
+        tokenBalance[_to] = tokenBalance[_to].add(_amount); 
+
+        return true;
+    }
+}
+
